cba3d2549ebe16b1f97fa9ac281ed58799d79d92
qipai <qipai12345@163.com>
2019-07-02 01:03:18 +0800

    添加了分享图片给朋友

--- app/src/main/java/com/yue/nn/MainActivity.java
+++ app/src/main/java/com/yue/nn/MainActivity.java
@@ -1,6 +1,12 @@
 package com.yue.nn;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+
+import com.tencent.mm.sdk.modelmsg.SendMessageToWX;
+import com.tencent.mm.sdk.modelmsg.WXImageObject;
+import com.tencent.mm.sdk.modelmsg.WXMediaMessage;
 import com.yue.nn.wxapi.AppConst;
 import com.yue.nn.wxapi.WXEntryActivity;
 import com.unity3d.player.UnityPlayerActivity;
@@ -24,4 +30,29 @@ public class MainActivity extends UnityPlayerActivity {
         WXEntryActivity.loginWeixin();
     }
 
+
+    public void shareImage(String path) {
+        // 初始化一个WXImageObject对象
+        WXImageObject imgObj = new WXImageObject();
+        imgObj.setImagePath(path);
+        // 用WXImageObject对象初始化一个WXMediaMessage对象
+        WXMediaMessage msg = new WXMediaMessage();
+        msg.mediaObject = imgObj;
+
+        // 图片压缩
+        Bitmap bmp = BitmapFactory.decodeFile(path);
+        Bitmap thumbBmp = Bitmap.createScaledBitmap(bmp, 150, 150, true);
+        bmp.recycle();
+
+        // 封装发送信息
+        SendMessageToWX.Req req = new SendMessageToWX.Req();
+        req.transaction = buildTransaction("img");
+        req.message = msg;
+        req.scene = SendMessageToWX.Req.WXSceneSession;
+        boolean issend = WXEntryActivity.api.sendReq(req);
+    }
+
+    private String buildTransaction(final String type) {
+        return (type == null) ? String.valueOf(System.currentTimeMillis()) : type + System.currentTimeMillis();
+    }
 }
